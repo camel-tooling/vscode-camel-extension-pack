ec69f93897503aa9f5c0cf8e8ff78c5a68c04ee6
Marcelo Henrique Diniz de Araujo <mdinizde@redhat.com>
2025-01-16 23:56:56 -0300

    FUSETOOLS2-2517: Adapt tests to use folder selection while creating a Camel project
    - Adapt tests to select a folder for the create camel project command.
    - Add a step in the github actions to allow openResources() to work
      properly on ubuntu 24.
    
    Signed-off-by: Marcelo Henrique Diniz de Araujo <mdinizde@redhat.com>

--- src/ui-test/variables.ts
+++ src/ui-test/variables.ts
@@ -9,9 +9,9 @@ export const DEMO_FILE = 'Demo.java';
 export const SPRINGBOOT_DIR = path.join(RESOURCES_DIR, 'springboot');
 export const SPRINGBOOT_CREATE_COMMAND = 'Camel: Create a Camel on SpringBoot project';
 export const SPRINGBOOT_ATTACH_DEBUGGER = 'Run Camel Spring Boot application and attach Camel debugger';
-export const SPRINGBOOT_PROJECT_FOLDER = path.join(SPRINGBOOT_DIR, 'src', 'main', 'java', 'com', 'acme', 'myproject');
+export const SPRINGBOOT_PROJECT_FOLDER = path.join(SPRINGBOOT_DIR, 'src', 'main', 'java', 'com', 'demo', 'test');
 
 export const QUARKUS_DIR = path.join(RESOURCES_DIR, 'quarkus');
 export const QUARKUS_CREATE_COMMAND = 'Camel: Create a Camel Quarkus project';
 export const QUARKUS_ATTACH_DEBUGGER = 'Run Camel Quarkus JVM application and attach Camel debugger';
-export const QUARKUS_PROJECT_FOLDER = path.join(QUARKUS_DIR, 'src', 'main', 'java', 'com', 'acme', 'myproject');
+export const QUARKUS_PROJECT_FOLDER = path.join(QUARKUS_DIR, 'src', 'main', 'java', 'com', 'demo', 'test');
